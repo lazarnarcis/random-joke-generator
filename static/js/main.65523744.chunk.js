1121768bd2bcff2dd8a9f57095574b7939098c94
Lazar Narcis <lnarcis310@gmail.com>
2021-11-30 21:27:30 +0200

    Updates

---
+++ static/js/main.65523744.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpapp1=this.webpackJsonpapp1||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);n(1);var r=n(3),c=n.n(r),s=n(0);function o(){return Object(s.jsx)(s.Fragment,{children:Object(s.jsx)("h1",{children:"Hello World..."})})}c.a.render(Object(s.jsx)(o,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.65523744.chunk.js.map
