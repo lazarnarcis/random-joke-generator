d9549ac6f689238d4fd54913362e7ce7a7a674bd
Lazar Narcis <lnarcis310@gmail.com>
2021-12-01 10:51:40 +0200

    Creating buttons and jokes in a JSON file (https://github.com/lazarnarcis/random-joke-generator/issues/11)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 import "./style.scss"
+import jokes from "./jokes.json"
 
 export default function App() {
+    const [randomJoke, setRandomJoke] = useState(null)
+    const generateJoke = () => {
+        setRandomJoke(Math.floor((Math.random() * jokes.length) + 1))
+    }
     return (
         <>
-            <h1>Hello World...</h1>
+            <h1>Random Joke Generator</h1>
+            <button type="button" onClick={() => generateJoke()}>Generate a new Joke</button>
+            <h3>Joke here:</h3>
+            <div>
+                {
+                    jokes.map(item => {
+                        return <p key={item.id}>{ randomJoke === item.id ? `${item.text}` : "" }</p>
+                    })
+                }
+            </div>
         </>
     )
 }
